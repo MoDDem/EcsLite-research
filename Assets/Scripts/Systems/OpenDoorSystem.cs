bd2b5567de0333ca90b2f47749ae9d2896d614f1
Daniel V <moddem25@gmail.com>
2022-05-12 22:23:52 +0500

    fin

--- Assets/Scripts/Systems/OpenDoorSystem.cs
+++ Assets/Scripts/Systems/OpenDoorSystem.cs
@@ -19,9 +19,19 @@ public class OpenDoorSystem : IEcsRunSystem
             ref var door = ref doorPool.Get(item);
             var pos = door.transform.localPosition;
             var startPos = door.startPosition;
+
+            //TODO: очень погано но я не знаю почему он так себя ведет, мало времени разобраться
+            if (door.transform.localRotation.eulerAngles.y == 90 || door.transform.localRotation.eulerAngles.y == -90)
+            {
+                door.transform.localPosition = Vector3.Lerp(pos, new Vector3(startPos.x + door.offset, startPos.y, startPos.z),
+                    0.3f * Time.deltaTime);
+            }
+            else
+            {
+                door.transform.localPosition = Vector3.Lerp(pos, new Vector3(startPos.x, startPos.y, startPos.z + door.offset),
+                    0.3f * Time.deltaTime);
+            }
             
-            door.transform.localPosition = Vector3.Lerp(pos, new Vector3(startPos.x, startPos.y, startPos.z+ door.offset),
-                0.3f * Time.deltaTime);
             
             if(Vector3.Distance(pos, startPos) >= door.offset)
                 openTagPool.Del(item);
